da4f0468b1a8d286f5101d0af341dffa7bea31af
woodsjc <woodsjc0@gmail.com>
2021-03-02 20:43:01 -0600

    Added basic files for routing paths. Next steps are adding async but probably shelve for a bit

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -13,7 +13,18 @@ impl WebsiteHandler {
 
     fn read_file(&self, file_path: &str) -> Option<String> {
         let path = format!("{}/{}", self.public_path, file_path);
-        fs::read_to_string(path).ok()
+
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {
+                    fs::read_to_string(path).ok()
+                } else {
+                    println!("Directory traversal attack attempted: {}", file_path);
+                    None
+                }
+            }
+            Err(_) => None
+        }
     }
 }
 
@@ -22,8 +33,11 @@ impl Handler for WebsiteHandler {
         match request.method() {
             Method::GET => match request.path() {
                 "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
-                "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello!</h1>".to_string())),
-                _ => Response::new(StatusCode::NotFound, None),
+                "/hello" => Response::new(StatusCode::Ok, self.read_file("hello.html")),
+                path => match self.read_file(path) {
+                    Some(contents) => Response::new(StatusCode::Ok, Some(contents)),
+                    None => Response::new(StatusCode::NotFound, None),
+                }
             }
             _ => Response::new(StatusCode::NotFound, None),
         }
